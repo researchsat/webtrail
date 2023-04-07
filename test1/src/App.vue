7af382bcc6205e8f717472eebc548f30b38bf10f
researchsat <raviteja@researchsat.com.au>
2023-04-07 14:27:17 +0930

    newfiles

---
+++ test1/src/App.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div id="app">
+    <NavBar />
+    <!-- other components or content -->
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style>
+
+</style>
+
---
+++ test1/src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">MyWebsite</b-navbar-brand>
+  
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+  
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/">Home</b-nav-item>
+          <b-nav-item href="/about">About</b-nav-item>
+          <b-nav-item href="/services">Services</b-nav-item>
+          <b-nav-item href="/contact">Contact</b-nav-item>
+          <b-nav-item href="/faq">FAQ</b-nav-item>
+        </b-navbar-nav>
+  
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown text="User" right>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Settings</b-dropdown-item>
+            <b-dropdown-divider></b-dropdown-divider>
+            <b-dropdown-item href="#">Logout</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </template>
+  
+  <script>
+  export default {
+    name: "NavBar",
+  };
+  </script>
+  
